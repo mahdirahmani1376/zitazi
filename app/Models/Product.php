0b7a758d0c4ffc9d25e120ec7ce69d4ef01f8c70
acer <rahmanimahdi16@gmail.com>
2025-02-13 20:15:10 +0330

    add command

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,41 @@
 
 namespace App\Models;
 
+use App\Enums\SourceEnum;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property string $own_id
+ * @property string $source_id
+ * @property string $source
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereOwnId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereSource($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereSourceId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Product whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
     protected $guarded = [
 
     ];
+
+    protected $casts = [
+        'source' => SourceEnum::class,
+    ];
+
+    public function belongsToTrendyol(): bool
+    {
+        return $this->source == SourceEnum::TRENDYOL;
+    }
 }
