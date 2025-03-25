becab1c40577445b001329664d68a6621cf01204
mahdi <rahmanimahdi16@gmail.com>
2025-03-25 23:41:30 +0330

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {
+    public function boot(): void
+    {
         Model::unguard();
     }
 }
