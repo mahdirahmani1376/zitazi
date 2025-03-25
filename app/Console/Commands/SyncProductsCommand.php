31035b5fedf08676d0f2111c24c433a3cedd6244
mahdi <rahmanimahdi16@gmail.com>
2025-03-25 18:51:31 +0330

    update

--- app/Console/Commands/SyncProductsCommand.php
+++ app/Console/Commands/SyncProductsCommand.php
@@ -172,6 +172,8 @@ class SyncProductsCommand extends Command
             'stock_status' => $product->stock > 0 ? 'instock' : 'outofstock',
         ];
 
+        Log::info("product_update_data_{$product->own_id}",$data);
+
         $response = $this->woocommerce->post("products/{$product->own_id}", $data);
         Log::info(
             "product_update_source_{$product->own_id}",
