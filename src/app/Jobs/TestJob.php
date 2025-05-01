fbe2d19f60067651b2d80e25b0d3d2d8f1fc5fdb
mahdi <rahmanimahdi16@gmail.com>
2025-05-01 13:40:51 +0330

    dockerized the app

--- src/app/Jobs/TestJob.php
+++ src/app/Jobs/TestJob.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
@@ -19,6 +20,7 @@ class TestJob implements ShouldQueue
     public function handle(): void
     {
         Log::info('test-job-success');
+        File::put(storage_path('logs/test.txt'), 't');
         Redis::set('test', 'Hello, Redis!');
         echo Redis::get('test');
     }
