c2af057c5139774dfe3b981a6697e6288d4735be
mahdi <rahmanimahdi16@gmail.com>
2025-05-07 22:31:15 +0330

    update

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -15,9 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withSchedule(function (Schedule $schedule) {
-        $schedule->command('db:seed')->dailyAt('05:00');
-        $schedule->command('app:sync-products')->dailyAt('06:00');
-        $schedule->command('app:sync-variations')->dailyAt('07:00');
+//        $schedule->command('db:seed')->dailyAt('05:00');
+//        $schedule->command('app:sync-products')->dailyAt('06:00');
+//        $schedule->command('app:sync-variations')->dailyAt('07:00');
 //        $schedule->command('app:sheet-report')->dailyAt('08:00');
         //        $schedule->command('app:index-zitazi-torob-products')->dailyAt('08:30');
     })
