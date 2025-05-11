b23638eb49b1359351ef9f25345f71c90c4f45bf
mahdi <rahmanimahdi16@gmail.com>
2025-05-11 13:56:14 +0330

    update

--- src/app/Imports/ImportDecathlonVariation.php
+++ src/app/Imports/ImportDecathlonVariation.php
@@ -6,16 +6,11 @@ use App\Models\Variation;
 use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Session;
 
 class ImportDecathlonVariation implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
-        if (!$this->validate($row)) {
-            return;
-        }
-
         try {
             return $this->updateVariationFromRow($row);
         } catch (\Exception $e) {
@@ -46,27 +41,4 @@ class ImportDecathlonVariation implements ToModel, WithHeadingRow
 
     }
 
-    private function validate($row): bool
-    {
-        $result = true;
-
-        if (empty($row['شناسه تنوع زیتازی'])) {
-            $result = false;
-        }
-
-        $variationCheck = Variation::where([
-            'own_id' => $row['شناسه تنوع زیتازی'],
-        ])->whereNot([
-            'sku' => $row['شناسه تنوع مرجع'],
-        ])->exists();
-
-        if ($variationCheck) {
-            $result = false;
-            Session::push('import_errors', [
-                'message' => 'شناسه تنوع زیتازی تکراری است: ' . $row['شناسه تنوع مرجع'],
-            ]);
-        }
-
-        return $result;
-    }
 }
