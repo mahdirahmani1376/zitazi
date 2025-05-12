b4f17e4bb3666ed752c63096f4115e7fb2319b68
mahdi <rahmanimahdi16@gmail.com>
2025-05-12 11:03:23 +0330

    update

---
+++ src/database/migrations/2025_05_12_105537_change_own_id_in_variations_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('variations', function (Blueprint $table) {
+            $table->dropUnique(['own_id']);
+        });
+    }
+
+    public function down(): void
+    {
+
+    }
+};
