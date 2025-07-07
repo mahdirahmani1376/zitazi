2f8f2ca47339e1ffb85f2b8209e0388d138dd5f4
mahdi <rahmanimahdi16@gmail.com>
2025-07-07 14:45:04 +0330

    update

--- src/app/Actions/TrendyolParser.php
+++ src/app/Actions/TrendyolParser.php
@@ -125,7 +125,7 @@ class TrendyolParser
             $element = $crawler->filter($element)->first();
             if ($element->count() > 0) {
                 $data = collect(json_decode($element->text(), true));
-                dump($data);
+                dump(json_encode($data));
                 $price = data_get($data, 'offers.price');
                 dump($price);
                 $price = floor((int)trim($price));
