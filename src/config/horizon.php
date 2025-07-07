ba29bf61608dd0c564c3f31c0ef5f267c8bac83b
mahdi <rahmanimahdi16@gmail.com>
2025-07-07 16:57:30 +0330

    update

--- src/config/horizon.php
+++ src/config/horizon.php
@@ -192,7 +192,7 @@ return [
             'maxTime' => 0,
             'maxJobs' => 0,
             'memory' => 128,
-            'tries' => 2,
+            'tries' => 1,
             'timeout' => 60,
             'nice' => 0,
         ],
