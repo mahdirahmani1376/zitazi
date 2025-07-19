bca8b1e416908f8b3effedd57cdc7fb15b77ec85
mahdi <rahmanimahdi16@gmail.com>
2025-07-19 13:32:22 +0330

    update

--- src/database/migrations/2025_07_19_094152_change_products_table.php
+++ src/database/migrations/2025_07_19_094152_change_products_table.php
@@ -8,14 +8,14 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->text('torob_source')->change();
+            $table->text('torob_source')->nullable()->change();
         });
     }
 
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('torob_source')->change();
+            $table->text('torob_source')->nullable()->change();
         });
     }
 };
