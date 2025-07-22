ef59c6a6d6c95e426da50950130b012d12b24296
mahdi <rahmanimahdi16@gmail.com>
2025-07-22 20:59:13 +0330

    update

--- src/app/Actions/SendHttpRequestAction.php
+++ src/app/Actions/SendHttpRequestAction.php
@@ -27,6 +27,7 @@ class SendHttpRequestAction
 
     public function sendWithCache($method, $url)
     {
+        dump($url);
         if (empty($headers)) {
             $headers = [
 //                'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.3',
