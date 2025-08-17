ab512018c269a58e5696c8ce4f970b7e1c372bd5
mahdi <rahmanimahdi16@gmail.com>
2025-08-17 19:43:01 +0330

    update

--- src/app/Actions/SyncVariationsActions.php
+++ src/app/Actions/SyncVariationsActions.php
@@ -9,15 +9,6 @@ class SyncVariationsActions
 {
     public function execute(Variation $variation): void
     {
-        try {
             BaseVariationCrawler::crawlVariation($variation);
-        } catch (\Exception $e) {
-            \Log::error('sync-variations-general-error',[
-                'variation_id' => $variation->id,
-                'error' => $e->getMessage(),
-                'trace' => $e->getTrace(),
-            ]);
-            dump($e->getMessage(),$variation->id);
-        }
     }
 }
