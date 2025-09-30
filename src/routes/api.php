e8acdd291aa00067dcc8a86b9c286940f9567337
mahdi <rahmanimahdi16@gmail.com>
2025-09-30 22:26:43 +0330

    update

--- src/routes/api.php
+++ src/routes/api.php
@@ -69,6 +69,7 @@ Route::get('decathlon-list', function () {
         'data' => Product::query()
             ->whereNot('decathlon_url', '=', '')
             ->orderBy('updated_at', 'asc')
+            ->whereIn('own_id', [698242, 787938])
             ->paginate()
     ]);
 });
