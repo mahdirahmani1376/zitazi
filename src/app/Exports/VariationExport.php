8b7a531bb27f663c43d87fba4fb4e7c79dd6200e
mahdi <rahmanimahdi16@gmail.com>
2025-11-01 16:47:41 +0330

    update

--- src/app/Exports/VariationExport.php
+++ src/app/Exports/VariationExport.php
@@ -40,23 +40,26 @@ class VariationExport implements FromQuery, WithHeadings, WithMapping
     public function map($row): array
     {
         /** @var variation $row */
-        return [
-            'id' => $row->id,
-            'product_id' => $row->product_id,
-            'product_name' => $row->product->product_name,
-            'zitazi_product_id' => $row->product->own_id,
-            'zitazi_variation_id' => $row->own_id,
-            'sku' => $row->sku,
-            'price' => $row->price,
-            'rial_price' => $row->rial_price,
-            'url' => $row->url,
-            'stock' => $row->stock,
-            'size' => $row->size,
-            'color' => $row->color,
-            'brand' => $row->product?->brand,
-            'source' => $row->source,
-            'updated_at' => $row->updated_at->toDateTimestring(),
-            'status' => $row->status,
-        ];
+        if (!empty($row)) {
+            return [
+                'id' => $row->id,
+                'product_id' => $row->product_id,
+                'product_name' => $row->product->product_name,
+                'zitazi_product_id' => $row->product->own_id,
+                'zitazi_variation_id' => $row->own_id,
+                'sku' => $row->sku,
+                'price' => $row->price,
+                'rial_price' => $row->rial_price,
+                'url' => $row->url,
+                'stock' => $row->stock,
+                'size' => $row->size,
+                'color' => $row->color,
+                'brand' => $row->product?->brand,
+                'source' => $row->source,
+                'updated_at' => $row->updated_at->toDateTimestring(),
+                'status' => $row->status,
+            ];
+        }
+
     }
 }
