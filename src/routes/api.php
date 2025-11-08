67eff89061af3118a77b10972bed1635b0ddd309
mahdi <rahmanimahdi16@gmail.com>
2025-11-08 18:20:06 +0330

    update

--- src/routes/api.php
+++ src/routes/api.php
@@ -69,9 +69,9 @@ Route::get('decathlon-list', function () {
         'data' => Product::query()
             ->whereNot('decathlon_url', '=', '')
             ->orderBy('updated_at', 'asc')
-//            ->whereIn('own_id', [
-//                844747
-//            ])
+            ->whereIn('own_id', [
+                844747
+            ])
             ->paginate()
     ]);
 });
@@ -81,7 +81,9 @@ Route::get('decathlon-list-retry', function () {
         'data' => Product::query()
             ->whereNot('decathlon_url', '=', '')
             ->orderBy('updated_at', 'asc')
-            ->whereDoesntHave('variations')
+            ->whereIn('own_id', [
+                844747
+            ])
             ->paginate()
     ]);
 });
