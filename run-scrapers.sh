e74f6e2a341836e5635c6a78a65e5c362cc1f98a
mahdi <rahmanimahdi16@gmail.com>
2025-11-08 18:19:48 +0330

    update

--- run-scrapers.sh
+++ run-scrapers.sh
@@ -2,7 +2,7 @@
 LOGFILE=/var/log/scraper.log
 echo "=== Scrape started at $(date) ===" >> $LOGFILE
 
-#node ~/projects/zitazi/node/src/scraper.js >> $LOGFILE 2>&1
-#node ~/projects/zitazi/node/src/scraper-retry.js >> $LOGFILE 2>&1
+node ~/projects/zitazi/node/src/scraper.js >> $LOGFILE 2>&1
+node ~/projects/zitazi/node/src/scraper-retry.js >> $LOGFILE 2>&1
 
 echo "=== Scrape finished at $(date) ===" >> $LOGFILE
