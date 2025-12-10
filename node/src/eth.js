aba5899f2dd67c73c48f1875427e813c1b991ef5
mahdi <rahmanimahdi16@gmail.com>
2025-12-10 10:56:17 +0330

    update

---
+++ node/src/eth.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer-extra');
+const stealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+puppeteer.use(stealthPlugin());
+
+(async () => {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: "new",
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.englishhome.com/magnet-pamuklu-cift-kisilik-battaniye-200x220-cm-krem-gri-23902', {waitUntil: 'networkidle2'});
+
+    let all = await page.content();
+    console.log('all', all);
+
+    const jsonData = await page.evaluate(() => {
+        const el = document.querySelector('script[type="application/ld+json"]');
+        if (!el) return null;
+        try {
+            return {
+                body: JSON.parse(el.textContent)
+            };
+        } catch {
+            return null;
+        }
+    });
+
+    let test = JSON.stringify(jsonData);
+
+    console.log(JSON.stringify(jsonData));
+
+    await browser.close();
+})();
