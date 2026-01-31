7162e9f12ca44f1799812bf428af355a4facd232
mahdi <rahmanimahdi16@gmail.com>
2026-01-31 23:13:48 +0330

    update

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -17,9 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withSchedule(function (Schedule $schedule) {
         $schedule->command('db:seed')->dailyAt('18:00')->withoutOverlapping();
+        $schedule->command('batch:sync-zitazi-products')->dailyAt('23:00')->withoutOverlapping();
         $schedule->command('app:sync-zitazi')->dailyAt('23:00')->withoutOverlapping();
         $schedule->command('app:sync-satreh')->dailyAt('23:00')->withoutOverlapping();
-        $schedule->command('batch:sync-zitazi-products')->dailyAt('23:00')->withoutOverlapping();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
