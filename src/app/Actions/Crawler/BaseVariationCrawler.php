52d4863c35f0a90970acbc906acc2a119d03c39e
mahdi <rahmanimahdi16@gmail.com>
2026-02-15 17:23:06 +0330

    update

--- src/app/Actions/Crawler/BaseVariationCrawler.php
+++ src/app/Actions/Crawler/BaseVariationCrawler.php
@@ -100,7 +100,7 @@ class BaseVariationCrawler
         try {
             $woocommerce = WoocommerceService::getClient($variation->base_source);
             $response = $woocommerce->post($url, $data);
-            LogManager::logVariation($variation, 'variation_update', [
+            LogManager::logVariation($variation, 'variation_zitazi-update-log', [
                 'body' => $data,
                 'response' => [
                     'price' => data_get($response, 'price'),
