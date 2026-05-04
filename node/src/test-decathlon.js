d09e303a0de6ef2589f10b0cb693340c5e2d8813
mahdi <rahmanimahdi16@gmail.com>
2026-05-04 16:30:13 +0330

    update

--- node/src/test-decathlon.js
+++ node/src/test-decathlon.js
@@ -1,28 +1,38 @@
 const puppeteer = require('puppeteer-extra');
-const stealthPlugin = require('puppeteer-extra-plugin-stealth');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
-puppeteer.use(stealthPlugin());
+puppeteer.use(StealthPlugin());
 
 (async () => {
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: "new",
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu"
+        ]
     });
+
     const page = await browser.newPage();
-    await page.goto('https://www.decathlon.com.tr/p/mikrofiber-havlu-l-boy-lila-80-130-cm/_/R-p-158325?mc=8732957&c=MAV%C4%B0', {waitUntil: 'networkidle2'});
+
+    await page.goto(
+        "https://www.decathlon.com.tr/p/mikrofiber-havlu-l-boy-lila-80-130-cm/_/R-p-158325?mc=8732957&c=MAV%C4%B0",
+        {waitUntil: "domcontentloaded"}
+    );
 
     const jsonData = await page.evaluate(() => {
         const el = document.querySelector('script[type="application/ld+json"]');
         if (!el) return null;
+
         try {
-            return {
-                body: JSON.parse(el.textContent)
-            };
-        } catch {
+            return JSON.parse(el.textContent);
+        } catch (e) {
             return null;
         }
     });
 
-    console.log(JSON.stringify(jsonData));
+    console.log(jsonData);
 
     await browser.close();
 })();
