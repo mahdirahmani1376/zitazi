01cd411b3966768d2eb2853cefeb64464e959b69
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-11 14:58:33 +0330

    update

--- src/app/Services/WoocommerceService.php
+++ src/app/Services/WoocommerceService.php
@@ -10,8 +10,8 @@ class WoocommerceService
     public static function getClient(string $source = Product::ZITAZI): Client
     {
         $baseURl = env('BASE_URL');
-        $securityKey = env('ck_e15335b8a32a1d69e70d58945979198de2de443e');
-        $securityPass = env('cs_5e59935a2584ea5c5d9aeb84ceb5d429f3df477f');
+        $securityKey = env('SECURITY_KEY');
+        $securityPass = env('SECURITY_PASS');
         if ($source === Product::SATRE) {
             $baseURl = env('SATRE_BASE_URL');
             $securityKey = env('SATRE_SECURITY_KEY');
