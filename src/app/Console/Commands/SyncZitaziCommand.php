bafa64bd0f415b51afce5ef8681ff7e717f477d4
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-11 04:07:37 +0330

    update

--- src/app/Console/Commands/SyncZitaziCommand.php
+++ src/app/Console/Commands/SyncZitaziCommand.php
@@ -26,56 +26,49 @@ class SyncZitaziCommand extends Command
 
         $jobs = [];
         foreach ($variations as $variation) {
-            $updateData = [];
-            if ($variation->status == Variation::AVAILABLE) {
-                $stock = $variation->stock;
-                $price = $variation->rial_price;
+            $stock = $variation->stock;
+            $price = $variation->rial_price;
 
-                if ($stock == 0) {
-                    $otherSource = null;
-                    if ($variation->source === Product::SOURCE_TRENDYOL) {
-                        $otherSource = Product::SOURCE_DECATHLON;
-                    } elseif ($variation->source === Product::SOURCE_DECATHLON) {
-                        $otherSource = Product::SOURCE_TRENDYOL;
-                    }
-
-                    if (!empty($otherSource)) {
-                        if ($variation->item_type === Product::VARIATION_UPDATE) {
-                            $otherSellerVariant =
-                                Variation::query()
-                                    ->where('own_id', $variation->own_id)
-                                    ->whereNot('id', $variation->id)
-                                    ->where('source', $otherSource)
-                                    ->where('item_type', Product::VARIATION_UPDATE)
-                                    ->first();
-                        } else {
-                            $otherSellerVariant =
-                                Variation::query()
-                                    ->where('product_id', $variation->product_id)
-                                    ->whereNot('id', $variation->id)
-                                    ->where('source', $otherSource)
-                                    ->where('item_type', Product::VARIATION_UPDATE)
-                                    ->first();
-                        }
-
-
-                        if (!empty($otherSellerVariant)) {
-                            $stock = $otherSellerVariant->stock;
-                            $price = $otherSellerVariant->rial_price;
-                        }
-                    }
-
+            if ($stock == 0) {
+                $otherSource = null;
+                if ($variation->source === Product::SOURCE_TRENDYOL) {
+                    $otherSource = Product::SOURCE_DECATHLON;
+                } elseif ($variation->source === Product::SOURCE_DECATHLON) {
+                    $otherSource = Product::SOURCE_TRENDYOL;
                 }
 
-                $updateData = ZitaziUpdateDTO::createFromArray([
-                    'stock_quantity' => $stock,
-                    'price' => $price,
-                ]);
-            } else {
-                $updateData = ZitaziUpdateDTO::createFromArray([
-                    'stock_quantity' => 0,
-                ]);
+                if (!empty($otherSource)) {
+                    if ($variation->item_type === Product::VARIATION_UPDATE) {
+                        $otherSellerVariant =
+                            Variation::query()
+                                ->where('own_id', $variation->own_id)
+                                ->whereNot('id', $variation->id)
+                                ->where('source', $otherSource)
+                                ->where('item_type', Product::VARIATION_UPDATE)
+                                ->first();
+                    } else {
+                        $otherSellerVariant =
+                            Variation::query()
+                                ->where('product_id', $variation->product_id)
+                                ->whereNot('id', $variation->id)
+                                ->where('source', $otherSource)
+                                ->where('item_type', Product::VARIATION_UPDATE)
+                                ->first();
+                    }
+
+
+                    if (!empty($otherSellerVariant)) {
+                        $stock = $otherSellerVariant->stock;
+                        $price = $otherSellerVariant->rial_price;
+                    }
+                }
+
             }
+
+            $updateData = ZitaziUpdateDTO::createFromArray([
+                'stock_quantity' => $stock,
+                'price' => $price,
+            ]);
 
             $jobs[] = new SyncZitaziJob($variation, $updateData);
         }
