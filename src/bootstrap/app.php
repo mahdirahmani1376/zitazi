e0b56f62224bac619b8d5d1f76502ef7075ff55f
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-21 16:01:25 +0330

    update

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('db:seed')->dailyAt('09:00')->withoutOverlapping();
 //        $schedule->command('batch:sync-zitazi-products')->dailyAt('23:00')->withoutOverlapping();
         $schedule->command('app:sync-zitazi')->dailyAt('23:00')->withoutOverlapping();
-//        $schedule->command('app:sync-satreh')->dailyAt('23:00')->withoutOverlapping();
+        $schedule->command('app:sync-satreh')->dailyAt('23:00')->withoutOverlapping();
         $schedule->command('model:prune')
             ->daily()
             ->at('00:00');
