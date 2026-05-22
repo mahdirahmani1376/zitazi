8d3ba580a2e6fedaab66bd6c7beb71a7da0ff979
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-22 16:01:41 +0330

    update

--- src/app/Models/LogModel.php
+++ src/app/Models/LogModel.php
@@ -2,20 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LogModel extends Model
 {
-    use Prunable;
+    use MassPrunable;
     protected $casts = [
         'data' => 'json'
     ];
 
     public function prunable()
     {
-        return static::where('created_at', '<=', now()->subDay());
+        return static::where('created_at', '<', now()->subWeek());
     }
 
     public function variation(): BelongsTo
