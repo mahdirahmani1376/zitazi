90d1a3defd6dd2afa703f8c2b2942c6b5d8fa08f
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-23 14:35:39 +0330

    update

--- run-scrapers.sh
+++ run-scrapers.sh
@@ -3,6 +3,7 @@
 echo "=== Scrape started at $(date) ==="
 
 /usr/bin/node node/src/scraper-tr.js
-
+/usr/bin/node node/src/scraper.js
+/usr/bin/node node/src/scraper-retry.js
 
 echo "=== Scrape finished at $(date) ==="
