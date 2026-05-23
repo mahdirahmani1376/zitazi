7d44db8f23c3199689f154185665c7ac9bd17984
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-23 14:22:49 +0330

    update

--- run-scrapers.sh
+++ run-scrapers.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-LOGFILE=/var/log/scraper.log
 
 truncate -s 0 "$LOGFILE"
 
-echo "=== Scrape started at $(date) ===" >> $LOGFILE
+echo "=== Scrape started at $(date) ==="
 
-/usr/bin/node node/src/scraper-tr.js >> $LOGFILE 2>&1
-/usr/bin/node node/src/scraper.js >> $LOGFILE 2>&1
-/usr/bin/node node/src/scraper-retry.js >> $LOGFILE 2>&1
+/usr/bin/node node/src/scraper-tr.js
 
-echo "=== Scrape finished at $(date) ===" >> $LOGFILE
+
+echo "=== Scrape finished at $(date) ==="
