3e98f97f36527d7e1e4b35381159af9dec72bd92
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-23 16:54:21 +0330

    update

--- node/ecosystem.config.js
+++ node/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
             script: "scraper-orchestrator.js",
             cwd: "/root/zitazi/node",
             interpreter: "node",
-            cron_restart: "* 15 * * *",
+            cron_restart: "0 15 * * *",
             autorestart: false,
             env: {
                 TZ: "Asia/Tehran"
