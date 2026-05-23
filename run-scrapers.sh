8b9deb489ecc3a340b98d55c01b80fa84a9c1b5d
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-23 13:13:31 +0330

    update

--- run-scrapers.sh
+++ run-scrapers.sh
@@ -5,8 +5,8 @@ truncate -s 0 "$LOGFILE"
 
 echo "=== Scrape started at $(date) ===" >> $LOGFILE
 
-node root/zitazi/node/src/scraper-tr.js >> $LOGFILE 2>&1
-node root/zitazi/node/src/scraper.js >> $LOGFILE 2>&1
-node root/zitazi/node/src/scraper-retry.js >> $LOGFILE 2>&1
+/usr/bin/node root/zitazi/node/src/scraper-tr.js >> $LOGFILE 2>&1
+/usr/bin/node root/zitazi/node/src/scraper.js >> $LOGFILE 2>&1
+/usr/bin/node root/zitazi/node/src/scraper-retry.js >> $LOGFILE 2>&1
 
 echo "=== Scrape finished at $(date) ===" >> $LOGFILE
