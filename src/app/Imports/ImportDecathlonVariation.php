db7bcb82e8fbc3907ecd5a852173036bd40fa6f3
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-05-31 16:46:52 +0330

    update

--- src/app/Imports/ImportDecathlonVariation.php
+++ src/app/Imports/ImportDecathlonVariation.php
@@ -1,75 +1,67 @@
 <?php
 
 namespace App\Imports;
-ini_set('memory_limit', '2G');
 
 use App\Actions\LogManager;
 use App\Models\Product;
 use App\Models\Variation;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Log;
+use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Row;
 
-class ImportDecathlonVariation implements ToModel, WithHeadingRow, WithChunkReading, ShouldQueue, SkipsEmptyRows
+class ImportDecathlonVariation implements OnEachRow, WithHeadingRow, WithChunkReading, ShouldQueue, SkipsEmptyRows
 {
     public function chunkSize(): int
     {
-        return 20;
+        return 100;
     }
-    public function model(array $row)
-    {
-        try {
-            return $this->updateVariationFromRow($row);
-        } catch (\Exception $e) {
-            Log::error('error-import', [
-                'error' => $e->getMessage(),
-                'row' => $row,
-            ]);
-        }
 
+    public function onRow(Row $row)
+    {
+        return $this->updateVariationFromRow($row->toArray());
     }
 
     public function updateVariationFromRow(array $row)
     {
         $result = Variation::where('id', $row['شناسه تنوع در وب سرویس'])->first();
-        $itemType = Product::VARIATION_UPDATE;
-        if (empty($row['شناسه تنوع زیتازی']) && empty($result->own_id)) {
-            $itemType = Product::PRODUCT_UPDATE;
-        }
-
-        if (!empty($result)) {
-
-            $oldOwnId = $result->own_id;
-            $oldIsDeleted = $result->is_deleted;
-            $oldItemType = $result->item_type;
-
-            $result->update([
-                'own_id' => $row['شناسه تنوع زیتازی'],
-                'item_type' => $itemType,
-                'is_deleted' => $row['غیرفعال'] ?? false
-            ]);
-
-            LogManager::logVariation($result, 'product-import-update', [
-                'own_id' => $row['شناسه تنوع زیتازی'],
-                'old_own_id' => $oldOwnId,
-                'item_type' => $itemType,
-                'old_item_type' => $oldItemType,
-                'is_deleted' => $row['غیرفعال'] ?? false,
-                'old_is_deleted' => $oldIsDeleted,
-            ]);
-
-            return $result;
-        } else {
+        if (empty($result)) {
             Log::error('product-import-update-not-found',
                 [
                     'id' => $row['شناسه تنوع در وب سرویس']
                 ]
             );
+            return $result;
         }
 
+        $itemType = Product::VARIATION_UPDATE;
+        if (empty($row['شناسه تنوع زیتازی']) && empty($result?->own_id)) {
+            $itemType = Product::PRODUCT_UPDATE;
+        }
+
+        $oldOwnId = $result->own_id;
+        $oldIsDeleted = $result->is_deleted;
+        $oldItemType = $result->item_type;
+
+        $result->update([
+            'own_id' => $row['شناسه تنوع زیتازی'],
+            'item_type' => $itemType,
+            'is_deleted' => $row['غیرفعال'] ?? false
+        ]);
+
+        LogManager::logVariation($result, 'product-import-update', [
+            'own_id' => $row['شناسه تنوع زیتازی'],
+            'old_own_id' => $oldOwnId,
+            'item_type' => $itemType,
+            'old_item_type' => $oldItemType,
+            'is_deleted' => $row['غیرفعال'] ?? false,
+            'old_is_deleted' => $oldIsDeleted,
+        ]);
+
+        return $result;
     }
 
 }
