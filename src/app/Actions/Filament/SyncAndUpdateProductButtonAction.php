0f9f43c25e83b7170c6b2298ab2203440030f6e3
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-06-02 22:34:41 +0330

    update

--- src/app/Actions/Filament/SyncAndUpdateProductButtonAction.php
+++ src/app/Actions/Filament/SyncAndUpdateProductButtonAction.php
@@ -2,52 +2,14 @@
 
 namespace App\Actions\Filament;
 
-use App\Jobs\SeedVariationsForProductJob;
+use App\Jobs\SendScrapeMessageJob;
 use App\Models\Product;
-use Illuminate\Support\Facades\Http;
 
 class SyncAndUpdateProductButtonAction
 {
-    public static function execute(Product $product, $bulk = false)
+    public static function execute(Product $product)
     {
-        info('sync button clicked for product: ' . $product->id);
-        if ($product->belongsToDecalthon()) {
-            $response = Http::post('http://172.18.0.1:3000/scrape', $product);
-            if (!$response->successful()) {
-                return [
-                    'success' => false,
-                ];
-            }
-            return [
-                'success' => true,
-            ];
-        } else if ($product->belongsToTrendyol()) {
-            $url = 'https://apigw.trendyol.com/discovery-storefront-trproductgw-service/api/product-detail/content';
-
-            $params = http_build_query([
-                'contentId' => $product->getTrendyolContentId(),
-                'merchantId' => $product->getTrendyolMerchantId(),
-            ]);
-
-            $product->full_url = $url . '?' . $params;
-
-            $response = Http::post('http://172.18.0.1:3000/scrape-tr', $product);
-            if (!$response->successful()) {
-                return [
-                    'success' => false,
-                ];
-            }
-            return [
-                'success' => true,
-            ];
-        } else {
-            if ($bulk) {
-                SeedVariationsForProductJob::dispatch($product, true);
-            } else {
-                SeedVariationsForProductJob::dispatchSync($product, true);
-                SendUpdateRequestForProductAction::execute($product);
-            }
-        }
+        SendScrapeMessageJob::dispatch($product);
 
         return [
             'success' => true,
