4db9bbfadd7b87f83152e1b31199e1d21a0b9a1e
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-06-02 22:29:12 +0330

    update

--- node/daemon.js
+++ node/daemon.js
@@ -7,7 +7,8 @@ const redis = new Redis({
 });
 
 // IMPORTANT: match Laravel prefix OR remove prefix in Laravel
-const QUEUE_IN = 'laravel_database_scrape_product';
+const QUEUE_IN_USER = 'laravel_database_scrape_bulk_product';
+const QUEUE_IN_BULK = 'laravel_database_scrape_product';
 const QUEUE_OUT = 'laravel_database_scrape_result';
 
 async function run() {
@@ -17,7 +18,11 @@ async function run() {
     while (true) {
 
         try {
-            const result = await redis.blpop(QUEUE_IN, 0);
+            const result = await redis.blpop(QUEUE_IN_USER, 0);
+            if (!result) {
+                await redis.blpop(QUEUE_IN_BULK, 0)
+            }
+
 
             const data = JSON.parse(result[1]);
 
