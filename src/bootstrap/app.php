e6cd383f8b886b4f7838c414f88cd05b364415a5
rahmanimahdi16@gmail.com <rahmanimahdi16@gmail.com>
2026-06-05 13:01:30 +0330

    update

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withSchedule(function (Schedule $schedule) {
         $schedule->command('db:seed --force')->dailyAt('09:00');
