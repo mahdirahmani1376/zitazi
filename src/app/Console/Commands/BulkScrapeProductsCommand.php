d26fd5b8fd3d93720599fa007d694ca21f235a0b
mahdi <rahmanimahdi16@gmail.com>
2026-08-28 17:14:11 +0330

    update

--- src/app/Console/Commands/BulkScrapeProductsCommand.php
+++ src/app/Console/Commands/BulkScrapeProductsCommand.php
@@ -2,75 +2,97 @@
 
 namespace App\Console\Commands;
 
-use App\Actions\LogManager;
-use App\Enums\SyncStatusEnum;
-use App\Models\Product;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Redis;
 
 class BulkScrapeProductsCommand extends Command
 {
-    protected $signature = 'app:bulk-scrape';
+    protected $signature = 'app:bulk-scrape {--source=}';
 
     protected $description = 'source options:[tr,de]';
 
     public function handle(): void
     {
-        Redis::pipeline(function ($pipe) {
-            foreach (Product::query()
-                         ->whereNotNull('trendyol_source')
-                         ->whereRaw("TRIM(trendyol_source) != ''")
-                         ->cursor() as $product) {
-                $product->setTrendyolFullUrl();
+        match ($this->option('source')) {
+            'tr' => $this->bulkSyncTrendyol(),
+            'de' => $this->bulkSyncDecathlon(),
+            default => $this->bulkSyncAll(),
+        };
+    }
 
-                if (empty($product->full_url)) {
-                    Log::error('no full_url for product', [
-                        'product_id' => $product->id,
-                        'trendyol_source' => $product->trendyol_source
-                    ]);
-                    return;
-                }
+    /**
+     * @return void
+     */
+    public function bulkSyncTrendyol(): void
+    {
+        dump('tren');
+//        Redis::pipeline(function ($pipe) {
+//            foreach (Product::query()
+//                         ->whereNotNull('trendyol_source')
+//                         ->whereRaw("TRIM(trendyol_source) != ''")
+//                         ->cursor() as $product) {
+//                $product->setTrendyolFullUrl();
+//
+//                if (empty($product->full_url)) {
+//                    Log::error('no full_url for product', [
+//                        'product_id' => $product->id,
+//                        'trendyol_source' => $product->trendyol_source
+//                    ]);
+//                    return;
+//                }
+//
+//                $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
+//
+//                LogManager::logProduct($product, 'product enqueued for batch processing');
+//
+//                $pipe->rpush(
+//                    config('queue.TR_QUEUE_IN'),
+//                    json_encode([
+//                        'product' => $product->only([
+//                            'id',
+//                            'full_url'
+//                        ]),
+//                        'bulk' => true,
+//                    ])
+//                );
+//            }
+//        });
+    }
 
-                $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
+    /**
+     * @return void
+     */
+    public function bulkSyncDecathlon(): void
+    {
+        dump('de');
+//        Redis::pipeline(function ($pipe) {
+//            foreach (Product::query()
+//                         ->whereNotNull('decathlon_url')
+//                         ->whereRaw("TRIM(decathlon_url) != ''")
+//                         ->cursor() as $product) {
+//
+//                $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
+//
+//                LogManager::logProduct($product, 'product enqueued for batch processing');
+//
+//                $pipe->rpush(
+//                    config('queue.DE_QUEUE_IN'),
+//                    json_encode([
+//                        'product' => $product->only([
+//                            'decathlon_url',
+//                            'id'
+//                        ]),
+//                        'bulk' => true,
+//                    ])
+//                );
+//            }
+//        });
+    }
 
-                LogManager::logProduct($product, 'product enqueued for batch processing');
+    private function bulkSyncAll(): void
+    {
+        dump('all');
 
-                $pipe->rpush(
-                    config('queue.TR_QUEUE_IN'),
-                    json_encode([
-                        'product' => $product->only([
-                            'id',
-                            'full_url'
-                        ]),
-                        'bulk' => true,
-                    ])
-                );
-            }
-        });
-
-        Redis::pipeline(function ($pipe) {
-            foreach (Product::query()
-                         ->whereNotNull('decathlon_url')
-                         ->whereRaw("TRIM(decathlon_url) != ''")
-                         ->cursor() as $product) {
-
-                $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
-
-                LogManager::logProduct($product, 'product enqueued for batch processing');
-
-                $pipe->rpush(
-                    config('queue.DE_QUEUE_IN'),
-                    json_encode([
-                        'product' => $product->only([
-                            'decathlon_url',
-                            'id'
-                        ]),
-                        'bulk' => true,
-                    ])
-                );
-            }
-        });
-
+        $this->bulkSyncDecathlon();
+        $this->bulkSyncTrendyol();
     }
 }
