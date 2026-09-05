7de2a8750d92f221c77f55dd072a7e7f807a54a7
mahdi <rahmanimahdi16@gmail.com>
2026-09-05 21:53:02 +0330

    update

--- src/app/Console/Commands/BulkScrapeProductsCommand.php
+++ src/app/Console/Commands/BulkScrapeProductsCommand.php
@@ -6,6 +6,7 @@ use App\Actions\LogManager;
 use App\Enums\SyncStatusEnum;
 use App\Models\Product;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 
@@ -15,6 +16,10 @@ class BulkScrapeProductsCommand extends Command
 
     protected $description = 'source options:[tr,de]';
 
+    public int $total = 0;
+    public int $enqueued = 0;
+    public int $invalid = 0;
+
     public function handle(): void
     {
         match ($this->option('source')) {
@@ -22,6 +27,22 @@ class BulkScrapeProductsCommand extends Command
             'de' => $this->bulkSyncDecathlon(),
             default => $this->bulkSyncAll(),
         };
+
+        Log::info('Bulk scrape chunk completed', [
+            'total' => $this->total,
+            'enqueued' => $this->enqueued,
+            'invalid' => $this->invalid,
+        ]);
+
+        $this->info('bulk scrape result');
+        $this->table(
+            ['Metric', 'Count'],
+            [
+                ['Total processed', $this->total],
+                ['Enqueued', $this->enqueued],
+                ['Invalid URL', $this->invalid],
+            ]
+        );
     }
 
     /**
@@ -29,32 +50,36 @@ class BulkScrapeProductsCommand extends Command
      */
     public function bulkSyncTrendyol(): void
     {
-        Redis::pipeline(function ($pipe) {
-            foreach (Product::query()
-                         ->whereNotNull('trendyol_source')
-                         ->whereRaw("TRIM(trendyol_source) != ''")
-                         ->cursor() as $product) {
-                if (empty($product->getTrendyolFullUrl())) {
-                    Log::error('no full_url for product', [
-                        'product_id' => $product->id,
-                        'trendyol_source' => $product->trendyol_source
-                    ]);
-                    return;
-                }
+        Product::query()
+            ->whereNotNull('trendyol_source')
+            ->whereRaw("TRIM(trendyol_source) != ''")
+            ->chunk(100, function (Collection $items) {
+                Redis::pipeline(function ($pipe) use ($items) {
+                    /** @var Product $item */
+                    foreach ($items as $product) {
+                        $this->total++;
 
-                $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
+                        $fullUrl = $product->getTrendyolFullUrl();
+                        if (empty($fullUrl)) {
+                            LogManager::logProduct($product, 'full url can not be set');
+                            $this->invalid++;
+                        } else {
+                            $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
+                            LogManager::logProduct($product, 'product enqueued for batch processing');
+                            $pipe->rpush(
+                                config('queue.TR_QUEUE_IN'),
+                                json_encode([
+                                    'product' => $product->getTrendyolQueueScrapeData(),
+                                    'bulk' => true,
+                                ])
+                            );
+                            $this->enqueued++;
+                        }
+                    }
 
-                LogManager::logProduct($product, 'product enqueued for batch processing');
+                });
+            });
 
-                $pipe->rpush(
-                    config('queue.TR_QUEUE_IN'),
-                    json_encode([
-                        'product' => $product->getTrendyolQueueScrapeData(),
-                        'bulk' => true,
-                    ])
-                );
-            }
-        });
     }
 
     /**
@@ -62,28 +87,33 @@ class BulkScrapeProductsCommand extends Command
      */
     public function bulkSyncDecathlon(): void
     {
-        Redis::pipeline(function ($pipe) {
-            foreach (Product::query()
-                         ->whereNotNull('decathlon_url')
-                         ->whereRaw("TRIM(decathlon_url) != ''")
-                         ->cursor() as $product) {
+        Product::query()
+            ->whereNotNull('decathlon_url')
+            ->whereRaw("TRIM(decathlon_url) != ''")
+            ->chunk(100, function (Collection $items) {
+                Redis::pipeline(function ($pipe) use ($items) {
+                    /** @var Product $item */
+                    foreach ($items as $product) {
+                        $this->total++;
+                        $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
+                        LogManager::logProduct($product, 'product enqueued for batch processing');
+                        $pipe->rpush(
+                            config('queue.DE_QUEUE_IN'),
+                            json_encode([
+                                'product' => $product->only([
+                                    'decathlon_url',
+                                    'id'
+                                ]),
+                                'bulk' => true,
+                            ])
+                        );
+                        $this->enqueued++;
 
-                $product->setSyncStatus(SyncStatusEnum::ENQUEUED, false);
+                    }
 
-                LogManager::logProduct($product, 'product enqueued for batch processing');
+                });
+            });
 
-                $pipe->rpush(
-                    config('queue.DE_QUEUE_IN'),
-                    json_encode([
-                        'product' => $product->only([
-                            'decathlon_url',
-                            'id'
-                        ]),
-                        'bulk' => true,
-                    ])
-                );
-            }
-        });
     }
 
     private function bulkSyncAll(): void
