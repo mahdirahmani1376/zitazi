77b478bf7e424d8e66b887933e3c2a2378d2b796
mahdi <rahmanimahdi16@gmail.com>
2026-09-21 13:33:31 +0330

    update

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -155,7 +155,7 @@ async function scrapeDecathlonData(productData) {
             };
         }
 
-        const delayTime = Math.floor(Math.random() * (5000 - 2000) + 10000);
+        const delayTime = Math.floor(Math.random() * (5000 - 2000) + 7000);
         await delay(delayTime);
 
         const elHandle = await page.waitForSelector(
