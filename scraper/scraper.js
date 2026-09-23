cdec44c458580925a8a3a53614c2ea86dc7041a6
mahdi <rahmanimahdi16@gmail.com>
2026-09-23 23:29:22 +0330

    update

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -56,6 +56,7 @@ let initialTrendyolTime = Date.now()
 
 let currentDecathlonTime = Date.now()
 let initialDecathlonTime = Date.now()
+let decathlonRequestCounter = 0;
 const BROWSER_RESTART_INTERVAL = 30 * 60 * 1000;
 
 async function beginScrape(name, data) {
@@ -209,6 +210,18 @@ async function scrapeDecathlonData(productData) {
             if (match) {
                 variation.size = match[1];
             }
+        }
+
+        decathlonRequestCounter++;
+        if (decathlonRequestCounter % 3 === 0) {
+            const batchPause = Math.floor(Math.random() * (30000 - 15000) + 15000);
+            console.log(JSON.stringify({
+                'message': 'decathlon batch pause',
+                'requests_completed': decathlonRequestCounter,
+                'pause_ms': batchPause,
+                'level': 'info'
+            }));
+            await delay(batchPause);
         }
 
         return {
